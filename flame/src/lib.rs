72741cc7889f32172c31d477af6c5d9d3915501e
Zachary Su <zach.su158@gmail.com>
2025-01-27 19:54:55 -0700

    added NULL instruction and change invoke native to take a static str

--- flame/src/lib.rs
+++ flame/src/lib.rs
@@ -1,9 +1,11 @@
+#[derive(Debug, Clone, PartialEq)]
 pub enum Instruction {
     Pushu8(u8),
     Pushf64(f64),
-    InvokeNative(String),
+    InvokeNative(&'static str),
     Pop(usize),
     Copy(Address, usize, Address),
+    NULL,
 }
 
 pub type Address = usize;
