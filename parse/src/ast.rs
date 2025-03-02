859a000fe4e8042acf565075d2c4d30624baedce
Zachary Su <zach.su158@gmail.com>
2025-03-02 00:21:13 -0700

    created FunDefinition

--- parse/src/ast.rs
+++ parse/src/ast.rs
@@ -1,8 +1,8 @@
-use std::iter::Peekable;
+use std::{iter::Peekable, marker::PhantomData};
 
-use token::Val;
+use token::{Val, Fun};
 
-use crate::{diagnostic, lex::{Colon, Equals, Ident, Literal, TokenTree}, Parse, Result};
+use crate::{diagnostic, lex::{Colon, Comma, Delimiter, Equals, Group, Ident, Literal, TokenTree}, Parse, Result};
 
 pub mod token;
 
@@ -24,6 +24,65 @@ macro_rules! parse_impl {
             }
         }
     };
+}
+
+macro_rules! delimiter {
+    ($name: literal : $(#[$meta: meta])* $vis: vis struct $ident: ident ( $delim: ident );) => {
+        $(#[$meta])*
+        $vis struct $ident<T>(T);
+
+        impl<T: Parse<TokenTree>> Parse<TokenTree> for $ident<T> {
+            fn parse<I>(iter: &mut Peekable<I>) -> Result<Self>
+            where I: Iterator<Item = TokenTree> + Clone
+            {
+                let group = Group::parse(iter)?;
+                if group.delimiter != Delimiter::$delim { return Err(diagnostic!(format!("expected {} group", $name)).into()); };
+                let iter = group.tokens.into_iter();
+                Ok(Self(T::parse(&mut iter.peekable())?))
+            }
+        }
+    };
+}
+
+pub trait DelimiterGroup {
+    fn parse_group<I>(iter: &mut Peekable<I>) -> Result<Group>
+    where I: Iterator<Item = TokenTree> + Clone;
+}
+
+delimiter!("paren":
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    pub struct ParenGroup(Paren);
+);
+
+delimiter!("bracket":
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    pub struct BracketGroup(Bracket);
+);
+
+delimiter!("brace":
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    pub struct BraceGroup(Brace);
+);
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Punctuated<T, P> {
+    _phantom: PhantomData<P>,
+
+    pub items: Vec<T>,
+}
+
+impl<T: Parse<TokenTree>, P: Parse<TokenTree>> Parse<TokenTree> for Punctuated<T, P> {
+    fn parse<I>(iter: &mut Peekable<I>) -> Result<Self>
+    where I: Iterator<Item = TokenTree> + Clone
+    {
+        let mut items = Vec::new();
+        items.push(T::parse(iter)?);
+        while iter.peek().is_some() {
+            P::parse(iter)?;
+            items.push(T::parse(iter)?);
+        };
+        Ok(Self { _phantom: PhantomData, items })
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -62,13 +121,22 @@ impl Parse<TokenTree> for LanternFile {
 #[derive(Debug, Clone, PartialEq)]
 pub enum Statement {
     ValDeclaration(ValDeclaration),
+    FunDefinition(FunDefinition),
 }
 
 impl Parse<TokenTree> for Statement {
     fn parse<I>(iter: &mut Peekable<I>) -> Result<Self>
     where I: Iterator<Item = TokenTree> + Clone
     {
-        Ok(Self::ValDeclaration(ValDeclaration::parse(iter)?))
+        let Some(peek) = iter.peek() else { panic!("unexpected end of iter"); };
+
+        if Val::is(peek) {
+            Ok(Self::ValDeclaration(ValDeclaration::parse(iter)?))
+        } else if Fun::is(peek) {
+            Ok(Self::FunDefinition(FunDefinition::parse(iter)?))
+        } else {
+            Err(diagnostic!("unexpected token '{peek:?}'").into())
+        }
     }
 }
 
@@ -81,6 +149,24 @@ parse_impl!(for TokenTree:
         pub r#type: Ident,
         pub equals: Equals,
         pub init: Expression,
+    }
+);
+
+parse_impl!(for TokenTree:
+    #[derive(Debug, Clone, PartialEq)]
+    pub struct FunDefinition {
+        pub fun: Fun,
+        pub ident: Ident,
+        pub args: ParenGroup<Punctuated<FunArg, Comma>>,
+    }
+);
+
+parse_impl!(for TokenTree:
+    #[derive(Debug, Clone, PartialEq)]
+    pub struct FunArg {
+        pub ident: Ident,
+        pub colon: Colon,
+        pub r#type: Ident,
     }
 );
 
