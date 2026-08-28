b4e2e761c0ca8060c00a85bd0096e16b5a4f94f0
DatAsianBoi123 <zach.su158@gmail.com>
2026-08-27 21:43:26 -0600

    fix: duplicate symbols in symbol table

--- diagnostic/src/symbol.rs
+++ diagnostic/src/symbol.rs
@@ -7,7 +7,7 @@ pub trait SymbolDisplay {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Symbol(usize);
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct SymbolTable<'a> {
     map: HashMap<&'a str, Symbol>,
     strings: Vec<&'a str>,
@@ -19,9 +19,12 @@ impl<'a> SymbolTable<'a> {
     }
 
     pub fn store(&mut self, str: &'a str) -> Symbol {
-        let id = self.strings.len();
-        self.strings.push(str);
-        *self.map.entry(str).or_insert(Symbol(id))
+        *self.map.entry(str)
+            .or_insert_with(|| {
+                let id = self.strings.len();
+                self.strings.push(str);
+                Symbol(id)
+            })
     }
 
     pub fn get(&self, str: &'a str) -> Option<Symbol> {
